22991394bad711272f467929f9a0791d3f9a7e81
Itta Anjali <160825054+ITTAANJALI@users.noreply.github.com>
2024-02-22 23:42:54 +0530

    Add files via upload

---
+++ array bubble sort.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int size;
+    printf("enter the size:");
+    scanf("%d",&size);
+    int arr[size];
+    for(int i=0;i<size;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int temp;
+    for(int i=0;i<size-1;i++)
+    {
+        for(int j=0;j<size-i-1;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+    printf("array elements are:");
+    for(int i=0;i<size;i++)
+    {
+        printf("%d",arr[i]);
+    }
+
+    return 0;
+}
