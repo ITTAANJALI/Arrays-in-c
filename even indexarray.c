c5f1ae4ecfd5003b14e3480f9ba782b5641bfdf2
Itta Anjali <160825054+ITTAANJALI@users.noreply.github.com>
2024-02-27 00:00:12 +0530

    Add files via upload

---
+++ even indexarray.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int size;
+    printf("enter the size:");
+    scanf("%d",&size);
+    int arr[size];
+    for(int i=0;i<size;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<size;i++)
+    {
+        if(i%2 ==0)
+        {
+            printf("%d",arr[i]);
+            
+        }
+    }
+
+    return 0;
+}
